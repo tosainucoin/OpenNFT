10b7e7df50e43f7840d4defbc5984eeaca139a7a
tosainucoin <31809486+tosainucoin@users.noreply.github.com>
2021-11-30 21:36:22 -0700

    Updated fronted

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,7 +1,7 @@
 export const ALGORAND_LEDGER = 'TestNet';
-export const USDC_ID = 14001707;
-export const USDC_DECIMAL_POINTS = 2;
-export const ASSET_URL = 'nft-market.ulam.io';
+export const USDC_ID = 400295506;
+export const USDC_DECIMAL_POINTS = 0;
+export const ASSET_URL = 'tosainuasa.com';
 export var BACKEND_URL;
 if (process.env.NODE_ENV === 'development') {
   BACKEND_URL = 'http://localhost:8000';
